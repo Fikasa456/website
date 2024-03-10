b56419c44bc0e494ec2f9336870da0c8b1736d95
ANAV <161984380+Fikasa456@users.noreply.github.com>
2024-03-10 02:57:25 +0200

    Add files via upload

---
+++ metadata.js
@@ -0,0 +1,7 @@
+{
+    "name":"YarnMyBody",
+    "description": "DJK",
+    "symbol": "ANUSS",
+    "decimals": 9,
+    "image": "https://cache.tonapi.io/imgproxy/-KqIo9vbp38QUuis2q_QOqsKVMPp2eBdpCV95CW4MC4/rs:fill:200:200:1/g:no/aHR0cHM6Ly9naXRodWIuY29tL0Zpa2FzYTQ1Ni93ZWJzaXRlL3Jhdy9tYWluL2ltYWdlLnBuZw.webp",
+}
